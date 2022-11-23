8e32eb7a8fe28fd06283785813e4751ecfdcb5ac
Liora Milbaum <lmilbaum@redhat.com>
2022-11-23 17:00:15 +0200

    feat(api): get secret value endpoint

--- coffre/main.py
+++ coffre/main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+
+@app.route("/api/secrets", methods=["GET"])
+def secrets():
+    "endpoint - /api/secrets?name=<secret name>"
+    if request.method == "GET":
+        name = request.args.get("name")
+        return {
+            "message": "This endpoint should return the secret value for "
+            f"secret named: {name}"
+        }
 
 
 @app.route("/")
